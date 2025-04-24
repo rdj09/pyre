e5db6cf2d6bcb99f7d5ec9e13b5fa839d29317c9
Rhys Jones <rhysjones437@googlemail.com>
2025-04-24 14:32:05 +0100

    adjustments made to relfect focus on datastructures

--- src/pyre/pricing.py
+++
@@ -1,6 +0,0 @@
-def calculate_premium(expected_loss, expense_ratio, profit_margin):
-    return expected_loss * (1 + expense_ratio + profit_margin)
-
-def calculate_risk_loading(std_dev_loss, confidence_level):
-    return std_dev_loss * confidence_level  # Simplified
-
