b4e7c5cb17f3e4d02923af4cac364a9e8ecdd89b
Rhys Jones <rhysjones437@googlemail.com>
2025-05-05 12:04:02 +0100

    iniitial work on treaty contracts classes

--- src/pyre/treaty/contracts.py
+++ src/pyre/treaty/contracts.py
@@ -1,38 +1,99 @@
+from enum import Enum
+from typing import List, Dict
+from dataclasses import dataclass
 
-class QuotaShareContract:
-    def __init__(self, share):
-        self.share = share
+class ContractType(Enum):
+    QUOTA_SHARE = "Quota Share"
+    EXCESS_OF_LOSS = "Excess of Loss"
+    SURPLUS_SHARE = "Surplus Share"
+    AGGREGATE_STOP_LOSS = "Aggregate Stop Loss"
 
-    def ceded_amount(self, loss):
-        return self.share * loss
+class ClaimTriggerBasis(Enum):
+    LOD = "Losses Occurrence" #Accident Year
+    RAD = "Risk Attaching" # Policy Inception Year
+    CMB = "Claims Made Basis" # Reported Year
 
-class ExcessOfLossContract:
-    def __init__(self, retention, limit):
-        self.retention = retention
-        self.limit = limit
+@dataclass
+class RILayer:
+    layer_id: int
+    layer_name: str 
+    layer_type: ContractType
+    occurrence_attachment:float
+    occurrence_limit:float
+    aggregate_attachment: float
+    aggregate_limit: float 
+    subject_lines_of_business: List
+    subject_lob_exposure_amounts: List
+    full_subject_premium: float
+    written_line: float
+    signed_line: float
+    number_of_reinstatements: int # unlimited 999 default need to build in logic elsewhere
+    reinstatement_cost: Dict # {1:100%,2:125%,3:125%} unlimited = 0% cost
+    cession: float = 1.0 # us for order and QS
 
-    def ceded_amount(self, loss):
-        if loss <= self.retention:
-            return 0
-        return min(loss - self.retention, self.limit)
+    @property
+    def written_line_premium(self) -> float:
+        return self.cession * self.written_line * self.full_subject_premium
+    
+    @property
+    def signed_line_premium(self) -> float:
+        return self.cession * self.signed_line * self.full_subject_premium
 
-class SurplusShareContract:
-    def __init__(self, retention):
-        self.retention = retention
+@dataclass
+class RIContractMetadata:
+    contract_id: int
+    contract_name: str
+    contract_type: ContractType
+    claim_trigger_basis: ClaimTriggerBasis
+    inception_date: str
+    expiration_date: str
+    fx_rates: Dict # {"GBP":1.0, "USD":1.25}
+    cedent_name: str   
 
-    def ceded_amount(self, sum_insured, loss):
-        if sum_insured <= self.retention:
-            return 0
-        surplus = sum_insured - self.retention
-        share = surplus / sum_insured
-        return share * loss
+class RIContract:
+    def __init__(self, contract_meta_data: RIContractMetadata, layers:List[RILayer]) -> None:
+        self._contract_meta_data = contract_meta_data
+        self._contract_layers = {layer.layer_id: layer for layer in layers}
 
-class AggregateStopLossContract:
-    def __init__(self, attachment_point, limit):
-        self.attachment_point = attachment_point
-        self.limit = limit
 
-    def ceded_amount(self, total_losses):
-        if total_losses <= self.attachment_point:
-            return 0
-        return min(total_losses - self.attachment_point, self.limit)
+
+
+
+
+# class QuotaShareContract:
+#     def __init__(self, share):
+#         self.share = share
+
+#     def ceded_amount(self, loss):
+#         return self.share * loss
+
+# class ExcessOfLossContract:
+#     def __init__(self, retention, limit):
+#         self.retention = retention
+#         self.limit = limit
+
+#     def ceded_amount(self, loss):
+#         if loss <= self.retention:
+#             return 0
+#         return min(loss - self.retention, self.limit)
+
+# class SurplusShareContract:
+#     def __init__(self, retention):
+#         self.retention = retention
+
+#     def ceded_amount(self, sum_insured, loss):
+#         if sum_insured <= self.retention:
+#             return 0
+#         surplus = sum_insured - self.retention
+#         share = surplus / sum_insured
+#         return share * loss
+
+# class AggregateStopLossContract:
+#     def __init__(self, attachment_point, limit):
+#         self.attachment_point = attachment_point
+#         self.limit = limit
+
+#     def ceded_amount(self, total_losses):
+#         if total_losses <= self.attachment_point:
+#             return 0
+#         return min(total_losses - self.attachment_point, self.limit)
