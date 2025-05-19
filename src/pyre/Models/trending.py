3c089f1cd5a5d38cdbd60bd3956bbf8185aeb9e5
Rhys Jones <rhysjones437@googlemail.com>
2025-05-19 15:59:51 +0100

    trending classes removed further consideration of trending model and other dynamics such as treaty terms.

--- src/pyre/Models/trending.py
+++ src/pyre/Models/trending.py
@@ -1,47 +1,13 @@
-from typing import Protocol
 from pyre.claims.claims import Claims
 from pyre.exposures.exposures import Exposures
 
-class Trender(Protocol):
-    def get_trend_factor(self, year: int) -> float:
-        ...
-    def trended_values(self, values: list[float], years: list[int]) -> list[float]:
-        ...
-    
-class ExposureTrender(Trender):
-    def __init__(self, exposures) -> None:
-        self._exposures = exposures
-        
-    @property
-    def exposures(self):
-        return self._exposures
-    
-    @exposures.setter
-    def exposures(self, values):
-        self._exposures = values
-    
-    @property
-    def trended_exposures(self):
-        #TODO apply trend factors to data
-        pass
-
-
-class ClaimTrender(Trender):
-    def __init__(self, claims:Claims) -> None:
-        self._claims = claims
-
-    @property
-    def claims(self):
-        return self._claims
-    
-    @claims.setter
-    def claims(self, values):
-        self._claims = values 
-        
-    def trended_claims(self):
-        #TODO #apply trend factors to the data
-
-        return Claims(...) 
+def trended_exposures(self) -> Exposures:
+    #TODO apply trend factors to data
+    return Exposures(...)
+      
+def trended_claims(self) -> Claims:
+    #TODO #apply trend factors to the data
+    return Claims(...) 
 
 
 #source citation: 
